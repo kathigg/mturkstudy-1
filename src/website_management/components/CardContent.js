a1c181466ae1a5b6c56ce9734a0d4ef6bfc5997d
VarunP09 <9varunpappu9@gmail.com>
2025-09-12 13:13:25 -0400

    Test Push

--- src/website_management/components/CardContent.js
+++ src/website_management/components/CardContent.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export const CardContent = ({ children }) => {
-  return <div className="text-gray-700">{children}</div>;
+  return <div className="blue">{children}</div>;
 };
+
+
+//return <div className="text-gray-700">{children}</div>;
