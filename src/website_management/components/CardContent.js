ca7d80d27eb0c61208a10b25d04812dcaebd16c0
VarunP09 <9varunpappu9@gmail.com>
2025-09-12 13:35:17 -0400

    Test2

--- src/website_management/components/CardContent.js
+++ src/website_management/components/CardContent.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 export const CardContent = ({ children }) => {
-  return <div className="blue">{children}</div>;
+  return <div className="text-red-700">{children}</div>;
 };
 
 
-//return <div className="text-gray-700">{children}</div>;
