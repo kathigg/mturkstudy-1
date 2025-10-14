10878fd16f9590b1b66080c271e38957813e0b97
VarunP09 <9varunpappu9@gmail.com>
2025-10-14 16:42:05 -0400

    Added Video & Fixed Completion Code Randomization Bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import NewsAnnotationTool from "./website_management/pages/NewsAnnotationTool.js";
+import instructionVid from "./Videos/Instruction-Video.mov";
 
 function App() {
   return (
     <div className="text-center">
+      
       <h1 className="text-2xl font-bold mt-4">News Annotation Tool</h1>
+      <video
+        src={instructionVid}
+        controls
+        autoPlay
+        muted
+        playsInline
+        width="600"
+        height="300"
+        className="block mx-auto"
+      />
+      
       <NewsAnnotationTool />
     </div>
   );
